96d907d047221ed2c768bd9c0527c06e3e87d0ee
ysryreal <oskarletowski1@gmail.com>
2025-05-25 20:11:53 +0100

    e

--- jsScripts/prxy.js
+++ jsScripts/prxy.js
@@ -2,6 +2,7 @@ const mainIframe = document.getElementsByClassName("mainIframe")[0]
 
 import { encode } from "./codec.js";
 
+
 export function goToLink(targetUrl)
 {   
     if (!targetUrl.startsWith("http://") && !targetUrl.startsWith("https://")) {
@@ -10,7 +11,7 @@ export function goToLink(targetUrl)
 
     const newUrl = encode(targetUrl)
 
-    const proxiedPath = __uv$config.prefix + newUrl;
+    const proxiedPath = "/service/" + newUrl;
     const fullUrl = location.origin + proxiedPath;
 
     mainIframe.src = fullUrl
