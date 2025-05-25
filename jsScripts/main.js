a79ea44cf797576fcd8712dd0371a190eb8662e1
ysryreal <oskarletowski1@gmail.com>
2025-05-25 20:06:13 +0100

    t

--- jsScripts/main.js
+++ jsScripts/main.js
@@ -7,42 +7,11 @@ const newTabButton = document.getElementById("newTabButton")
 const urlInputForm = document.getElementById("topSearchBarForm")
 const urlInput = document.getElementById("topSearchBarInput")
 
-import { goToLink, generateLink } from "./prxy.js"
-
-goToLink("https://discord.com")
-
-mainbackButton.addEventListener("click", () => {
-    let nextTab = getUrlById(getId() - 1)
-
-    console.log(nextTab)
-
-    if (nextTab !== undefined)
-    {
-        setId(getId() - 1)
-        goToLink(nextTab)
-    }
-})
-
-mainforwardButton.addEventListener("click", () => {
-    let nextTab = getUrlById(getId() + 1)
-
-    if (nextTab !== undefined)
-    {   
-        setId(getId() + 1)
-        goToLink(nextTab)
-    }
-})
-
-mainRefreshButton.addEventListener("click", () => {
-    goToLink(getUrlById(getId()))
-})
+import { goToLink } from "./prxy.js"
 
 urlInputForm.addEventListener("submit", function(event)
 {
     event.preventDefault()
 
-    generateLink(urlInput.value)
-
     goToLink(urlInput.value)
-    pushNewLink(urlInput.value)
 })
